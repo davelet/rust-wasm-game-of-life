0a303aef710c9d5824ddee4454553c789fb34506
Sheldon <Sheldon.Wei.-ND@disney.com>
2024-09-14 15:08:21 +0800

    game of life

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,4 @@
 mod utils;
+mod world;
+mod entity;
 
-use wasm_bindgen::prelude::*;
-
-#[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, wasm-game-of-life!{}", name));
-}
